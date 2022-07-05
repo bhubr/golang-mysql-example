a92573fa904b03e78b149159294f6c4cc415aed4
Benoît Hubert <benoithubert@gmail.com>
2022-07-05 22:07:49 +0200

    add grant query

--- init.sql
+++ init.sql
@@ -1,2 +1,3 @@
-CREATE USER golang@localhost IDENTIFIED BY 'golang';
+CREATE USER 'golang'@'localhost' IDENTIFIED BY 'golang';
+GRANT ALL PRIVILEGES ON testdb.* to 'golang'@'localhost';
 CREATE DATABASE testdb;
